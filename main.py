93baff262be209dfa7c379cc03fe7ad6283162ca
DARSH <129121284+DARSHTRON@users.noreply.github.com>
2023-06-14 16:24:50 +0300

    Change on the loading system

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import os
 bot = discord.Bot(intents=discord.Intents.all())
 
 def load_cogs():
-    for folder in os.listdir("cogs"):
-        if not "." in folder:
-            bot.load_extension(f"cogs.{folder}.__init__")
+    # Loading the cogs like this kinda better O.O
+    [bot.load_extension(f"cogs.{folder}.__init__") for folder in os.listdir("cogs") if not "." in folder]
 
 
 load_cogs()
